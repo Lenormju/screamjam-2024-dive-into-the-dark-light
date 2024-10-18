abf21acfef3e2fe42ed48265404f3b961082ebf7
Lou BEGE <lou.bege@kaizen-solutions.net>
2024-10-18 21:34:14 +0200

    Move + Cam

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float SpeedPlayer = 3;
+    public float SensibilityCamera = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, z);
+        transform.Translate(movement.normalized * SpeedPlayer * Time.deltaTime);
+
+        float horizontalCam = SensibilityCamera * Input.GetAxis("Mouse X");
+        transform.Rotate(0, horizontalCam, 0);
+    }
+}
