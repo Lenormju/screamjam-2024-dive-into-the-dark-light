228e899c448fb34e91472ab6f851101e3160939a
Julien LENORMAND <lenormand.julien0@gmail.com>
2024-10-19 18:20:54 +0200

    audio raycasting

---
+++ Assets/Scripts/AudioSourceRaycasting.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class AudioSourceRaycasting : MonoBehaviour
+{
+    private Transform _player_transform;
+    private AudioSource _audio_source;
+
+    void Start() {
+        _player_transform = GameObject.Find("/Player").transform;
+        _audio_source = GetComponent<AudioSource>();
+    }
+    void Update() {
+        Vector3 origin = this.transform.position;
+        Vector3 direction = _player_transform.position - origin;
+
+        RaycastHit hit;
+        bool has_hit_something = Physics.Raycast(origin, direction, out hit);  // from AudioSource to Player
+
+        bool is_player_visible = false;  // by default
+        if (has_hit_something) {
+            //Debug.Log("hit something: ", hit.transform);
+            if (hit.transform == _player_transform) {
+                //Debug.Log("hit player");
+                is_player_visible = true;
+            } else {
+                //Debug.Log("hit something else than player");
+            }
+        } else {
+            //Debug.Log("pas hit");
+        }
+
+        if (is_player_visible) {
+            this._audio_source.volume = 0.5F;
+        } else {
+            this._audio_source.volume = 0.1F;
+        }
+    }
+}
