b5ba7d63bd9aaa6770473228d6f3b8b6e0eaff67
Julien LENORMAND <lenormand.julien0@gmail.com>
2024-10-20 16:17:48 +0200

    add door locked sound

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -12,6 +12,7 @@ public class Door : MonoBehaviour
     public AudioSource doorAudioSource;
     public AudioClip doorOpeningSound;
     public AudioClip doorClosingSound;
+    public AudioClip doorLockedSound;
 
 
     [SerializeField] private int nb_keys_needed = 0;
@@ -33,8 +34,12 @@ public class Door : MonoBehaviour
             if (!is_door_open)
             {
                 pushAmount += amountOnClick;
-                if (pushAmount >= amountToOpen && GameManager.Instance.nb_keys >= nb_keys_needed)
+                if (GameManager.Instance.nb_keys < nb_keys_needed) {
+                    //Debug.Log("pas assez de keys, still locked");
+                    doorAudioSource.PlayOneShot(doorLockedSound);
+                } else if (pushAmount >= amountToOpen && GameManager.Instance.nb_keys >= nb_keys_needed)
                 {
+                    //Debug.Log("enough keys and push, will open");
                     GameManager.Instance.RemoveKey(nb_keys_needed);
                     animator.SetTrigger("openingDoor");
                     is_door_open = true;
@@ -43,12 +48,13 @@ public class Door : MonoBehaviour
             }
             else if (pushAmount <= 0)
             {
+                //Debug.Log("closing");
                 animator.SetTrigger("closingDoor");
                 is_door_open = false;
                 doorAudioSource.PlayOneShot(doorClosingSound);
             }
         }
-        if (pushAmount > 0)
+        if (pushAmount > 0)  // losing push while time passes (QTE)
         {
             pushAmount -= Time.deltaTime * losingOverTimeAmount;
             if (pushAmount < 0)
@@ -56,13 +62,13 @@ public class Door : MonoBehaviour
                 pushAmount = 0;
             }
         }
-        if (PushSlider != null && !is_door_open && amountToOpen > 0 && pushAmount > 0)
+        if (PushSlider != null && !is_door_open && amountToOpen > 0 && pushAmount > 0)  // start QTE
         {
             PushSlider.gameObject.SetActive(true);
             PushSlider.value = pushAmount;
             GameManager.Instance.StartBangingDoorMusic();
         }
-        else if (PushSlider != null)
+        else if (PushSlider != null) // stop QTE
         {
             PushSlider.gameObject.SetActive(false);
         }
